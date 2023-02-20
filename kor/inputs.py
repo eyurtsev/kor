9cd1f4bff42664b36798603e804c30827b01f76e
Eugene Yurtsev <eyurtsev@gmail.com>
2023-02-20 16:12:50 -0500

    Update

--- kor/inputs.py
+++ kor/inputs.py
@@ -2,52 +2,17 @@
 import dataclasses
 from typing import Sequence
 
-FLIGHT_BOOKING = (
-    "You are interacting with a user who is attempting to buy a flight ticket. "
-    "They need to specify an origin airport or "
-    "city, a destination airport or city and a date or time for the flight. The user may also "
-    "specify a price range for the tickets. Please read the user input and extract the relevant "
-    "pieces of information. Format the extracted information between appropriate opening and "
-    "closing tags (e.g., <origin>LAX</origin>). If no information can be found, output an empty "
-    "string between for the given tag.\n "
-    "\n"
-    "Use only the tags: <destination>, <origin>, <time>, <price-min>, <price-max>\n"
-    "\n"
-    "Input: I want to fly at 8:00 AM 2011-01-01 to Hawaii.\n"
-    "Output: <time>8:00 AM 2011-01-01</time>, <destination>Hawaii</destination>\n"
-    "\n"
-    "Input: Book me a flight from TLV to BOS on 06/06\n"
-    "Output: <origin>TLV</origin>, <destination>BOS</destination>, <time>06/06</time>\n"
-    "\n"
-    "Input: Flight from LAX between $100-$200\n"
-    "Output: <origin>LAX</origin>,<price-min>$100</price-min>,<price-max>$200</price-max>\n"
-    "\n"
-    "Input: {input}\n"
-    "Output:\n"
-)
 
-FLIGHT_PROGRESS = """
-"You are interacting with a user who is attempting to buy a flight ticket. "
-"They need to specify an origin airport or "
-"city, a destination airport or city and a date or time for the flight. The user may also "
-"specify a price range for the tickets. Please read the user input and extract the relevant "
-"pieces of information. Format the extracted information between appropriate opening and "
-"closing tags (e.g., <origin>LAX</origin>). If no information can be found, output an empty "
-"string between for the given tag.\n "
-"\n"
-"Use only the tags: <destination>, <origin>, <time>, <price-min>, <price-max>\n"
-"\n"
-"Input: I want to fly at 8:00 AM 2011-01-01 to Hawaii.\n"
-"Output: <time>8:00 AM 2011-01-01</time>, <destination>Hawaii</destination>\n"
-"\n"
-"Input: Book me a flight from TLV to BOS on 06/06\n"
-"Output: <origin>TLV</origin>, <destination>BOS</destination>, <time>06/06</time>\n"
+@dataclasses.dataclass(frozen=True)
+class Input:
+    id: str  # Unique ID
+    examples: Sequence[str]
 
-You've already collected some information from the user. The user can specify that they
-want to update a given piece of information or add more missing information. 
 
-Input: {input}
-"""
+@dataclasses.dataclass(frozen=True)
+class DateInput(Input):
+    description: str
+    format: str
 
 
 @dataclasses.dataclass(frozen=True)
@@ -78,14 +43,6 @@ class Selection:
 
 
 @dataclasses.dataclass(frozen=True)
-class DateInput:
-    id: str
-    description: str
-    format: str
-    examples: Sequence[str]
-
-
-@dataclasses.dataclass(frozen=True)
 class Form:
     id: str
     description: str
@@ -95,11 +52,30 @@ class Form:
 CHOICE_TYPE = "RADIO"
 
 
+def compile_examples(input: Input):
+    formatted_examples = []
+
+    for example in input.examples:
+        formatted_examples.extend([f"Input: {example}", f"Output: <{option.id}>"])
+
+
 def date_input_block(input: str, date_input: DateInput) -> str:
     """Get prompt for parsing a date input."""
-    prompt = """
-    You are interacting with a user. Your goal is to
-    """
+
+    prompt = (
+        "The input may or may not contain a date. If it contains a date, please "
+        " include it in the output inside of <date> and </date> tags, and report it"
+        "in ISO-8601 format (YYYY-MM-DD). If it doesn't contain a date, please "
+        "output <null/>. Do not output anything else.\n"
+        "\n"
+        "Input: I went to the store on January 7th, 2023.\n"
+        "Output: <date>2023-01-07</date>\n"
+        "Input: Next \n"
+        "Output: <date>2023-01-07</date>\n"
+        "Input: {input}\n"
+        "Output: ",
+    )
+    return prompt
 
 
 def choice_block(input: str, kind: str, options: Sequence[Option]) -> str:
---
+++ kor/llm_utils.py
@@ -0,0 +1,6 @@
+"""Stiching code to work with LLMs."""
+
+
+def parse_llm_response(response: str):
+    """Parse llm response."""
+    raise NotImplemented()
