67ba0ba8a1d42c639c6a32fb6a14362e54615502
Eugene Yurtsev <eyurtsev@gmail.com>
2023-03-08 13:56:12 -0500

    Update

--- kor/example_generation.py
+++ kor/example_generation.py
@@ -1,26 +1,28 @@
-"""Module for code that generates examples for a given form.
+"""Module for code that generates examples for a given input.
 
 At the moment, this code only has a simple implementation that concatenates all the
 examples, but one may want to select or generate examples in a smarter way, or take
 into account the finite size of the context window and limit the number of examples.
 """
-from typing import List, Sequence, Any
+from typing import List, Sequence, Any, Union
 
 from kor.elements import Form, Selection, ExtractionInput, AbstractInput
 
+LITERAL_TYPE = Union[str, int, float]
 
-def _write_single_tag(tag_name: str, value: str) -> str:
-    """Write a tag."""
+
+def _write_literal(tag_name: str, value: LITERAL_TYPE) -> str:
+    """Write literal."""
     return f"<{tag_name}>{value}</{tag_name}>"
 
 
-def _write_list(tag_name: str, values: Sequence[str]) -> str:
+def _write_list(tag_name: str, values: Sequence[LITERAL_TYPE]) -> str:
     """Write list."""
     return "".join(_write_tag(tag_name, value) for value in values)
 
 
-def _write_complex_tag(tag_name: str, data: dict[str, str]) -> str:
-    """Write a complex tag."""
+def _write_dict(tag_name: str, data: dict[str, Any]) -> str:
+    """Write a dict."""
     s_data = "".join(
         [
             _write_tag(key, value)
@@ -30,14 +32,17 @@ def _write_complex_tag(tag_name: str, data: dict[str, str]) -> str:
     return _write_tag(tag_name, s_data)
 
 
-def _write_tag(tag_name: str, data: str | Sequence[str] | dict[str, Any]) -> str:
+def _write_tag(
+    tag_name: str, data: LITERAL_TYPE | Sequence[LITERAL_TYPE] | dict[str, Any]
+) -> str:
     """Write a tag."""
-    if isinstance(data, (str, int, float, complex)):
-        return _write_single_tag(tag_name, data)
+    # Dispatch based on type.
+    if isinstance(data, (str, int, float)):
+        return _write_literal(tag_name, data)
     elif isinstance(data, list):
         return _write_list(tag_name, data)
     elif isinstance(data, dict):
-        return _write_complex_tag(tag_name, data)
+        return _write_dict(tag_name, data)
     else:
         raise NotImplementedError(f"No support for {tag_name}")
 
@@ -45,7 +50,7 @@ def _write_tag(tag_name: str, data: str | Sequence[str] | dict[str, Any]) -> str
 # PUBLIC API
 
 
-def _generate_selection_examples(selection: Selection, encoding: str) -> list[tuple[str, str]]:
+def _generate_selection_examples(selection: Selection) -> list[tuple[str, str]]:
     """Generate examples for a given selection input."""
     formatted_examples = []
     for option in selection.options:
@@ -59,7 +64,7 @@ def _generate_selection_examples(selection: Selection, encoding: str) -> list[tu
 
 
 def _generate_extraction_input_examples(
-    extraction_input: ExtractionInput, encoding: str
+    extraction_input: ExtractionInput,
 ) -> list[tuple[str, str]]:
     """List of 2-tuples of input, output.
 
@@ -75,11 +80,11 @@ def _generate_extraction_input_examples(
     return formatted_examples
 
 
-def _generate_examples_form(form: Form, encoding: str) -> List[tuple[str, str]]:
+def _generate_examples_form(form: Form) -> List[tuple[str, str]]:
     """Generate examples form."""
     examples = []
     for element in form.elements:
-        element_examples = generate_examples(element, encoding=encoding)
+        element_examples = generate_examples(element)
         # If the form is to be interpreted as a coherent object, then
         # we do a trick and wrap all the outputs in the form ID.
         if form.as_object:  # Wrap all examples in a parent tag
@@ -104,16 +109,38 @@ def _generate_examples_form(form: Form, encoding: str) -> List[tuple[str, str]]:
     return examples
 
 
-def generate_examples(element: AbstractInput, encoding: str = 'XML') -> List[tuple[str, str]]:
-    """Dispatch based on element type."""
+# PUBLIC API
+
+
+def generate_examples(
+    element: AbstractInput, encoding: str = "XML"
+) -> List[tuple[str, str]]:
+    """Generate examples for a given element.
+
+    A rudimentary implementation that simply concatenates all available examples
+    from the components across the entire element tree.
+
+    Does not provide a way to impose constraints (e.g., select a subset of examples
+    to meet a constraint on the overall number of tokens.)
+
+    Args:
+        element: AbstractInput
+        encoding: Reserved parameter, refers to the encoding of the output, it's unclear
+                  whether or how different encodings affect the ability of LLMs to match
+                  a given schema
+
+
+    Returns:
+        list of 2-tuples containing input, output pairs
+    """
+    if encoding != "XML":
+        raise NotImplementedError("Only XML encoding is supported right now.")
+    # Dispatch based on element type.
     if isinstance(element, Form):
-        return _generate_examples_form(element, encoding=encoding)
+        return _generate_examples_form(element)
     elif isinstance(element, Selection):
-        return _generate_selection_examples(element, encoding=encoding)
+        return _generate_selection_examples(element)
     elif isinstance(element, ExtractionInput):  # Catch all
-        return _generate_extraction_input_examples(element, encoding=encoding)
+        return _generate_extraction_input_examples(element)
     else:
         raise NotImplementedError(f"No support for {type(element)}")
-
-
-simple_example_generator = generate_examples
