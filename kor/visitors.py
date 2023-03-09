4df89b898b826b5ee6097086bda1e9104e3b992f
Eugene Yurtsev <eyurtsev@gmail.com>
2023-03-09 10:10:18 -0500

    update

--- kor/visitors.py
+++ kor/visitors.py
@@ -11,10 +11,13 @@ class TypePrinter(AbstractVisitor[None]):
     def __init__(self) -> None:
         """Use to print the type."""
         self.depth = 0
+        self.type_str_messages = []
 
     def visit_default(self, node: "AbstractInput") -> None:
         space = "* " + self.depth * " "
-        print(f"{space}{node.id}: {node.__class__.__name__} # {node.description}")
+        self.type_str_messages.append(
+            f"{space}{node.id}: {node.__class__.__name__} # {node.description}"
+        )
 
     def visit_object(self, node: ObjectInput) -> None:
         self.visit_default(node)
@@ -22,3 +25,36 @@ class TypePrinter(AbstractVisitor[None]):
         for child in node.elements:
             child.accept(self)
         self.depth -= 1
+
+    def get_type(self) -> str:
+        """Get the type."""
+        return "\n".join(self.type_str_messages)
+
+
+class TypeScriptType(AbstractVisitor[None]):
+    def __init__(self) -> None:
+        """Use to print the type."""
+        self.depth = 0
+        self.code_lines = []
+
+    def visit_default(self, node: "AbstractInput") -> None:
+        space = self.depth * " "
+        self.code_lines.append(
+            f"{space}{node.id}: {node.__class__.__name__} // {node.description}"
+        )
+
+    def visit_object(self, node: ObjectInput) -> None:
+        space = self.depth * " "
+
+        self.code_lines.append(f"{space}{node.id}: {{")
+
+        self.depth += 1
+        for child in node.elements:
+            child.accept(self)
+        self.depth -= 1
+
+        self.code_lines.append(f"{space}}}")
+
+    def get_type_description(self) -> str:
+        """Get the type."""
+        return "\n".join(self.code_lines)
