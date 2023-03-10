07f8b4d92d60c585cb2988937295db314e7c319a
Eugene Yurtsev <eyurtsev@gmail.com>
2023-03-10 14:31:13 -0500

    Udpate

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -83,4 +83,10 @@ html_context = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    "css/custom.css",
+]
+
 nb_execution_mode = "off"
