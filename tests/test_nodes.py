3a8d93670af05b3a2f064bccf4fc1529cc692b5d
Eugene Yurtsev <eyurtsev@gmail.com>
2023-03-11 10:14:00 -0500

    Delete some code, fix tests, bump min python versin (#21)
    * Bump to python 3.10 for minimal version (at least for now)
    * Remove some old code for the time being
    * Clean up tests

--- tests/test_nodes.py
+++ tests/test_nodes.py
@@ -1,19 +1,25 @@
+from typing import Any
+
 import pytest
 
 from kor import nodes
+from kor.nodes import AbstractVisitor
 
 
-class ToyInput(elements.AbstractInput):
+class ToyInput(nodes.AbstractInput):
     """Toy input for tests."""
 
+    def accept(self, visitor: AbstractVisitor) -> Any:
+        raise NotImplemented()
 
-@pytest.mark.parametrize("invalid_id", ["", "@@#", " "])
+
+@pytest.mark.parametrize("invalid_id", ["", "@@#", " ", "NAME", "1name", "name-name"])
 def test_invalid_identifier_raises_error(invalid_id: str) -> None:
     with pytest.raises(ValueError):
         ToyInput(id=invalid_id, description="Toy")
 
 
-@pytest.mark.parametrize("valid_id", ["name", "NAME", "name-name", "_name", "1name"])
+@pytest.mark.parametrize("valid_id", ["name", "name_name", "_name", "n1ame"])
 def test_can_instantiate_with_valid_id(valid_id) -> None:
     """Can instantiate an abstract input with a valid ID."""
     ToyInput(id=valid_id, description="Toy")
@@ -21,4 +27,5 @@ def test_can_instantiate_with_valid_id(valid_id) -> None:
 
 def test_extraction_input_cannot_be_instantiated() -> None:
     """ExtractionInput is abstract and should not be instantiated."""
-    elements.ExtractionInput(id="help", description="description", examples=[])
+    with pytest.raises(TypeError):
+        nodes.ExtractionInput(id="help", description="description", examples=[])
