48f7f77a47e31543ab2bdad6fe687c1ce759bdbe
Eugene Yurtsev <eyurtsev@gmail.com>
2023-03-14 13:28:15 -0400

    update

--- kor/examples.py
+++ kor/examples.py
@@ -6,7 +6,8 @@ into account the finite size of the context window and limit the number of examp
 
 The code uses a default encoding of XML. This encoding should match the parser.
 """
-from typing import Any, List, Sequence, Tuple, Union
+import json
+from typing import Any, List, Sequence, Tuple, Union, TypeVar
 
 from kor.nodes import (
     AbstractInput,
@@ -56,18 +57,43 @@ def _write_tag(
         raise NotImplementedError(f"No support for {tag_name}")
 
 
+T = TypeVar("T")
+
+
 class SimpleExampleGenerator(AbstractVisitor[List[Tuple[str, str]]]):
+    def __init__(self, encoding: str):
+        """Initialize the simple example generator."""
+        self.encoding = encoding
+
     def visit_option(self, node: "Option") -> List[Tuple[str, str]]:
+        """Should not visit Options directly."""
         raise AssertionError("Should never visit an Option node.")
 
+    def _xml_encoder(self, key: str, data):
+        if isinstance(data, str) and not data.strip():
+            return _write_tag(key, "")
+        return _write_tag(key, data)
+
+    def _encode(self, node: AbstractInput, data) -> Any:
+        """Apply an encoder."""
+        if self.encoding == "XML":
+            return self._xml_encoder(node.id, data)
+        elif self.encoding in {"plain", "JSON"}:
+            if node.multiple and not isinstance(data, (tuple, list)):
+                data = [data]
+            return {node.id: data}
+        else:
+            raise NotImplementedError()
+
     def visit_object(self, node: "Object") -> List[Tuple[str, str]]:
+        """Implementation of an object visitor."""
         if node.group_as_object and node.examples:
             object_examples = [
                 # Looks like false positive from mypy
                 # Can investigate how to simplify at a later point.
                 (
                     example_input,
-                    _write_tag(node.id, example_output),  # type: ignore[arg-type]
+                    self._encode(node, example_output),  # type: ignore[arg-type]
                 )
                 for example_input, example_output in node.examples
             ]
@@ -81,7 +107,7 @@ class SimpleExampleGenerator(AbstractVisitor[List[Tuple[str, str]]]):
             child_examples = child.accept(self)
             if node.group_as_object:  # Take care of namespaces
                 child_examples = [
-                    (example_input, _write_tag(node.id, example_output))
+                    (example_input, self._encode(node, example_output))
                     for example_input, example_output in child_examples
                 ]
 
@@ -90,10 +116,11 @@ class SimpleExampleGenerator(AbstractVisitor[List[Tuple[str, str]]]):
         return examples
 
     def visit_selection(self, node: "Selection") -> List[Tuple[str, str]]:
+        """Selection visitor."""
         examples = []
         for option in node.options:
             for example in option.examples:
-                examples.append((example, _write_tag(node.id, option.id)))
+                examples.append((example, self._encode(node, option.id)))
 
         for null_example in node.null_examples:
             examples.append((null_example, ""))
@@ -106,11 +133,19 @@ class SimpleExampleGenerator(AbstractVisitor[List[Tuple[str, str]]]):
         examples = []
 
         for text, extraction in node.examples:
-            if isinstance(extraction, str) and not extraction.strip():
-                value = ""
-            else:
-                value = _write_tag(node.id, extraction)
+            value = self._encode(node, extraction)
             examples.append((text, value))
+        return examples
+
+    def visit(self, node: "AbstractInput") -> List[Tuple[str, str]]:
+        """Entry-point."""
+        examples = node.accept(self)
+        if self.encoding == "JSON":
+            return [
+                (input_example, json.dumps(output_example))
+                for input_example, output_example in examples
+            ]
+
         return examples
 
 
@@ -138,9 +173,4 @@ def generate_examples(
     Returns:
         list of 2-tuples containing input, output pairs
     """
-    if encoding != "XML":
-        raise NotImplementedError("Only XML encoding is supported right now.")
-
-    example_generator = SimpleExampleGenerator()
-    examples = element.accept(example_generator)
-    return examples
+    return SimpleExampleGenerator(encoding=encoding).visit(element)
--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,4 +1,7 @@
+import pytest
+
 from kor.examples import _write_tag, generate_examples
+from typing import List
 from kor.nodes import Number, Object, Option, Selection, Text
 
 
@@ -47,3 +50,62 @@ def test_example_generation() -> None:
     assert examples[0] == ("another number", "<object><number>1</number></object>")
     assert examples[1] == ("number", "<object><number>2</number></object>")
     assert examples[2] == ("text", "<object><text>3</text></object>")
+
+
+@pytest.mark.parametrize(
+    "encoding,expected",
+    [
+        (
+            "plain",
+            [
+                ("another number", {"object": [{"number": ["1"]}]}),
+                ("number", {"object": [{"number": ["2"]}]}),
+                ("text", {"object": [{"text": ["3"]}]}),
+                ("selection", {"object": [{"selection": ["option"]}]}),
+                ("foo", {"object": [""]}),  # TODO(Eugene): This is a bug
+                ("1 2", {"object": [{"age": ["1", "2"]}]}),
+            ],
+        ),
+        (
+            "JSON",
+            [
+                ("another number", '{"object": [{"number": ["1"]}]}'),
+                ("number", '{"object": [{"number": ["2"]}]}'),
+                ("text", '{"object": [{"text": ["3"]}]}'),
+                ("selection", '{"object": [{"selection": ["option"]}]}'),
+                ("foo", '{"object": [""]}'),  # TODO(Eugene): This is a bug
+                ("1 2", '{"object": [{"age": ["1", "2"]}]}'),
+            ],
+        ),
+    ],
+)
+def test_example_generation_with_plain_encoding(
+    encoding: str, expected: List[dict]
+) -> None:
+    """Light test for plain encoding.
+
+    Verifies that examples are getting picked up and encoded properly.
+    """
+    option = Option(id="option", description="Option", examples=["selection"])
+    number = Number(id="number", description="Number", examples=[("number", "2")])
+    age = Number(id="age", description="Age", examples=[("1 2", ["1", "2"])])
+    text = Text(id="text", description="Text", examples=[("text", "3")])
+
+    selection = Selection(
+        id="selection",
+        description="Selection",
+        options=[option],
+        null_examples=["foo"],
+    )
+
+    obj = Object(
+        id="object",
+        description="object",
+        examples=[("another number", {"number": ["1"]})],
+        attributes=[number, text, selection, age],
+    )
+
+    examples = generate_examples(obj, encoding=encoding)
+    assert isinstance(examples, list)
+    # Verify a few generated examples
+    assert examples == expected
