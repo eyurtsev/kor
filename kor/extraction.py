1675ada2776e3b3393b982629bc28296346f6d0e
Harrison Chase <hw.chase.17@gmail.com>
2023-03-15 07:24:07 -0700

    First pass at langchain integration
    First pass at langchain integraiton

--- kor/extraction.py
+++ kor/extraction.py
@@ -1,16 +1,21 @@
 import abc
-from typing import Dict, List, Union
+from typing import Dict, List
+
+from langchain.schema import BaseLanguageModel
 
 from kor import nodes
-from kor.llms import ChatCompletionModel, CompletionModel
 from kor.parsing import parse_llm_output
-from kor.prompts import STANDARD_EXTRACTION_TEMPLATE, PromptGenerator
+from kor.prompts import (
+    STANDARD_EXTRACTION_TEMPLATE,
+    ExtractionPromptValue,
+    PromptGenerator,
+)
 
 
 class Extractor(abc.ABC):  # pylint: disable=too-few-public-methods
     def __init__(
         self,
-        model: Union[CompletionModel, ChatCompletionModel],
+        model: BaseLanguageModel,
         prompt_generator: PromptGenerator = STANDARD_EXTRACTION_TEMPLATE,
     ) -> None:
         """Initialize an extractor with a model and a prompt generator."""
@@ -21,15 +26,9 @@ class Extractor(abc.ABC):  # pylint: disable=too-few-public-methods
         self, user_input: str, node: nodes.AbstractInput
     ) -> Dict[str, List[str]]:
         """Invoke the extractor with a user input and a schema node."""
-        prompt: Union[str, List[Dict[str, str]]]
-        if isinstance(self.model, CompletionModel):
-            prompt = self.prompt_generator.format_as_string(user_input, node)
-        elif isinstance(self.model, ChatCompletionModel):
-            prompt = self.prompt_generator.format_as_chat(user_input, node)
-        else:
-            raise NotImplementedError(
-                f"Unsupported model interface for type {type(self.model)}."
-            )
-        # Looks like a false positive from mypy, but I'm not sure why.
-        model_output = self.model(prompt)  # type: ignore[arg-type]
-        return parse_llm_output(model_output)
+        prompt = ExtractionPromptValue(
+            template=self.prompt_generator, user_input=user_input, node=node
+        )
+        model_output = self.model.generate_prompt([prompt])
+        text = model_output.generations[0][0].text
+        return parse_llm_output(text)
