4c11214ba63382beccba70b1f617f7c6ca51bba5
Eugene Yurtsev <eyurtsev@gmail.com>
2023-03-17 10:06:01 -0400

    Update

--- kor/extraction.py
+++ kor/extraction.py
@@ -1,14 +1,55 @@
 import abc
-from typing import Dict, List
+from typing import Dict, List, Any
 
-from langchain.schema import BaseLanguageModel
+from pydantic import Extra
 
 from kor import nodes
+from kor.encoders import Encoder
+from kor.nodes import AbstractInput
 from kor.prompts import (
     STANDARD_EXTRACTION_TEMPLATE,
     ExtractionPromptValue,
     PromptGenerator,
 )
+from langchain import BasePromptTemplate
+from langchain.output_parsers import BaseOutputParser
+from langchain.schema import BaseLanguageModel, PromptValue
+
+
+class ExtractionPromptTemplate(BasePromptTemplate):
+    node: AbstractInput
+
+    def format_prompt(self, user_input: str, **kwargs: Any) -> PromptValue:
+        """Format the prompt."""
+        return ExtractionPromptValue(
+            template=STANDARD_EXTRACTION_TEMPLATE, user_input=user_input, node=self.node
+        )
+
+    def format(self, **kwargs: Any) -> str:
+        raise NotImplementedError()
+
+    @property
+    def _prompt_type(self) -> str:
+        """Prompt type."""
+        return "ExtractionPromptTemplate"
+
+
+class KorEncoder(BaseOutputParser):
+    """A langchain parser """
+    encoder: Encoder
+
+    @property
+    def _type(self) -> str:
+        return 'KOREn'
+
+    def parse(self, text: str):
+        return self.encoder.decode(text)
+
+    class Config:
+        """Configuration for this pydantic object."""
+
+        extra = Extra.forbid
+        arbitrary_types_allowed = True
 
 
 class Extractor(abc.ABC):  # pylint: disable=too-few-public-methods
