acdeb845f6d49c5efe6b1c9d3744438e565c6c83
Eugene Yurtsev <eyurtsev@gmail.com>
2023-03-17 21:39:41 -0400

    asd

--- kor/encoders/csv_data.py
+++ kor/encoders/csv_data.py
@@ -4,9 +4,8 @@ The code will need to eventually support handling some form of nested objects,
 via either JSON encoded column values or by breaking down nested attributes
 into additional columns (likely both methods).
 """
-import csv
 from io import StringIO
-from typing import Any, Dict, List, Mapping, Sequence, cast
+from typing import Any, Dict, List
 
 import pandas as pd
 
@@ -16,49 +15,12 @@ from kor.nodes import AbstractInput, Object
 DELIMITER = "|"
 
 
-def _encode(fieldnames: Sequence[str], data: Sequence[Mapping[str, Any]]) -> str:
-    """Encode CSV."""
-    with StringIO() as string_io:
-        writer = csv.DictWriter(string_io, fieldnames, delimiter=DELIMITER)
-        writer.writeheader()
-        writer.writerows(data)
-        encoded_string = string_io.getvalue()
-    return encoded_string
-
-
-def _decode(fieldnames: Sequence[str], csv_text: str) -> List[Dict[str, Any]]:
-    """Decode CSV text using the given fieldnames."""
-    with StringIO(csv_text) as s:
-        reader = csv.DictReader(s, fieldnames=fieldnames, delimiter=DELIMITER)
-        header = cast(Dict[str, str], next(reader))
-
-        if set(header.values()) != set(fieldnames):
-            raise ValueError(
-                f"Difference between expected fieldnames: {fieldnames} and header"
-                f" {header.values()}"
-            )
-        return list(row for row in reader)
-
-
 def _extract_top_level_fieldnames(node: AbstractInput) -> List[str]:
     """Temporary schema description for CSV extraction."""
     if isinstance(node, Object):
         return [attributes.id for attributes in node.attributes]
     else:
         return [node.id]
-
-
-def _assert_object_is_supported(node: AbstractInput) -> None:
-    """Assert that the node is an object and that it has only flat attributes."""
-    if not isinstance(node, Object):
-        return
-
-    for attribute in node.attributes:
-        if attribute.many or isinstance(attribute, Object):
-            raise AssertionError(
-                "CSV Encoder does not yet support embedded lists or "
-                f"objects (attribute `{attribute.id}`)."
-            )
 
 
 # PUBLIC API
@@ -70,7 +32,16 @@ class CSVEncoder(Encoder):
     def __init__(self, node: AbstractInput) -> None:
         """Attach node to the encoder to allow the encoder to understand schema."""
         super().__init__(node)
-        _assert_object_is_supported(self.node)
+
+        # Verify that if we have an Object then none of its attributes are lists or objects
+        # as that functionality is not yet supported.
+        if isinstance(node, Object):
+            for attribute in node.attributes:
+                if attribute.many or isinstance(attribute, Object):
+                    raise AssertionError(
+                        "CSV Encoder does not yet support embedded lists or "
+                        f"objects (attribute `{attribute.id}`)."
+                    )
 
     def encode(self, data: Any) -> str:
         """Encode the data."""
@@ -117,11 +88,11 @@ class CSVEncoder(Encoder):
     def get_instruction_segment(self) -> str:
         """Format instructions."""
         return (
-            "Please output the extracted information in CSV format in Excel"
-            " dialect.Only output the table. Precede the table with a <table> tag and"
-            " use a closing tag </table> after the table.Do not output anything except"
-            " for the table. Do not add any clarifying information. All output must be"
-            " in CSV format and follow the schema specified above. Do not add any"
-            " additional columns that do not appear in the schema. If the attribute is"
-            " an array or an object, please JSON encode it."
+            "Please output the extracted information in CSV format in Excel dialect."
+            " Only output the table. Output an opening <table> tag before the table and"
+            " a closing </table> after the table. Do not output anything except for the"
+            " table. Do not add any clarifying information. Output must follow the"
+            " schema above. Do not add any additional columns that do not appear in the"
+            " schema. If a column corresponds to an array or an object, use a JSON"
+            " encoding to encode its value."
         )
