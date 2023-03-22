eb070798f44fd33a1ae83fbfc515747dfcd865f6
Eugene Yurtsev <eyurtsev@gmail.com>
2023-03-22 14:05:40 -0400

    update

--- tests/test_public_interface.py
+++ tests/test_public_interface.py
@@ -7,15 +7,15 @@ def test_kor__all__() -> None:
 
     Upon changes this may help serve as a reminder to correctly bump the semver.
     """
-    assert __all__ == (
-        "Text",
-        "Object",
-        "Number",
+    assert sorted(__all__) == [
+        "BulletPointDescriptor",
         "create_extraction_chain",
-        "TypeDescriptor",
-        "TypeScriptTypeGenerator",
-        "BulletPointTypeGenerator",
         "CSVEncoder",
-        "XMLEncoder",
         "JSONEncoder",
-    )
+        "Number",
+        "Object",
+        "Text",
+        "TypeDescriptor",
+        "TypeScriptDescriptor",
+        "XMLEncoder",
+    ]
