5dbf105abbe4fb84206b7301c8a6f5e6807c5211
Eugene Yurtsev <eyurtsev@gmail.com>
2023-03-30 11:57:46 -0400

    Update

--- kor/processing/html.py
+++ kor/processing/html.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup
 from langchain.document_loaders.base import BaseLoader
 from langchain.schema import Document
 from langchain.text_splitter import TextSplitter, RecursiveCharacterTextSplitter
+from playwright.async_api import async_playwright
+from playwright.sync_api import sync_playwright
 from typing import Union, Sequence, List, Optional
 
 
@@ -16,28 +18,19 @@ def _get_mini_html(content: str) -> str:
     for stylesheet in soup.find_all("link", rel="stylesheet"):
         stylesheet.extract()
 
-    # Remove all image elements
-    for img in soup.find_all("img"):
-        img.extract()
+    elements_to_remove = ("img", "path", "svg", "style")
 
-    # Remove all path elements
-    for path in soup.find_all("path"):
-        path.extract()
-
-    # Remove all SVG elements
-    for svg in soup.find_all("svg"):
-        svg.extract()
-
-    # Remove all style tags
-    for style in soup.find_all("style"):
-        style.extract()
+    for element_to_remove in elements_to_remove:
+        # Remove all image elements
+        for img in soup.find_all(element_to_remove):
+            img.extract()
 
     new_html = repr(soup)
     return new_html
 
 
 def _convert_html(html: str) -> str:
-    """Clean up HTML."""
+    """Clean up HTML and convert to markdown using markdownify."""
     html = _get_mini_html(html)
     # return html
     md = markdownify.markdownify(html)
@@ -47,12 +40,49 @@ def _convert_html(html: str) -> str:
 ## PUBLIC API
 
 
+def download_html(url: str) -> str:
+    """Download HTML from a URL."""
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=True)
+        page = browser.new_page()
+        page.goto(url)
+
+        # Wait for the page to finish rendering
+        html = page.content()
+        browser.close()
+    return html
+
+
+async def a_download_html(url: str) -> str:
+    """Download an HTML from a URL."""
+    with async_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=True)
+        page = browser.new_page()
+        page.goto(url)
+
+        # Wait for the page to finish rendering
+        html = page.content()
+        browser.close()
+    return html
+
+
 class HTMLToMarkDown(BaseLoader):
     """A loader that converts HTML to Markdown."""
 
     def __init__(self, texts: Union[Sequence[str], Sequence[Document]]) -> None:
         """Convert HTML to markdown."""
         self.texts = texts
+
+    @classmethod
+    def from_url(cls, url: str, run_javascript: bool = False) -> "HTMLToMarkDown":
+        """Load HTML from a URL."""
+        import requests
+
+        if run_javascript:
+            html = download_html(url)
+        else:
+            html = requests.get(url).text
+        return cls(texts=[html])
 
     def load(self) -> List[Document]:
         """Load data into document objects."""
