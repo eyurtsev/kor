bf1f7f6e9cfb824a3a3b27b3e4e032256e918e31
Eugene Yurtsev <eyurtsev@gmail.com>
2023-04-04 13:45:38 -0400

    update

---
+++ tests/test_prompt_generation.py
@@ -0,0 +1,35 @@
+import pytest
+
+from kor.prompts import create_langchain_prompt
+from kor import TypeScriptDescriptor, JSONEncoder
+from kor.encoders import InputFormatter
+from kor import Object
+
+
+@pytest.mark.parametrize(
+    "input_formatter, expected_string",
+    [
+        (None, 'user input'),
+        ("triple_quotes", '"""\nuser input\n"""'),
+        ("text_prefix", 'Text: """\nuser input\n"""'),
+    ],
+)
+def test_input_formatter_applied_correctly(
+    input_formatter: InputFormatter, expected_string: str
+) -> None:
+    untyped_object = Object(
+        id="obj",
+        examples=[("text", {"text": "text"})],
+        attributes=[],
+    )
+    prompt = create_langchain_prompt(
+        untyped_object,
+        JSONEncoder(),
+        TypeScriptDescriptor(),
+        input_formatter=input_formatter,
+    )
+
+    prompt_value = prompt.format_prompt(text="user input")
+
+    assert prompt_value.to_messages()[-1].content == expected_string
+    assert expected_string in prompt_value.to_string()
